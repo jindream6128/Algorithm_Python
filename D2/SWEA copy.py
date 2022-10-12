a3b705b109f84e412d2a9e017f9d0157aa2b6083
jindream6128 <jindream6128@naver.com>
2022-10-13 00:03:42 +0900

    d2 day4

---
+++ D2/SWEA copy.py
@@ -0,0 +1,10 @@
+T = int(input())
+for test_case in range(1,T+1):
+    N, K = map(int,input().split())
+    [list(map(int,input().split())) for _ in range(N)]
+    
+
+
+    https://www.youtube.com/watch?v=WnU7jXEpLXY
+
+    https://www.notion.so/ko-kr/templates/college-student-dashboard
