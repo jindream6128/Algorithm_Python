2bdee9d18ed5555249c918832e1c6d1f6e140926
jindream6128 <jindream6128@naver.com>
2022-11-06 19:46:17 +0900

    d3 1106

--- D3/SWEA 1215_palindreome.py
+++ D3/SWEA 1215_palindreome.py
@@ -1,8 +1,26 @@
 # file input
-# import sys
-# sys.stdin = open("input.txt", "r")
+import sys
+sys.stdin = open("input.txt", "r")
 
-T = int(input())
+for tc in range(1, 11):
+    N = int(input())  # 찾아야하는 회문의 길이 N
+    arr = [list(input()) for _ in range(8)]  # 단어 입력 8*8
+    ans = 0
 
-for test_case in range(1, T+1):
-    N =int(input())
+    for i in range(0,8):
+        for j in range(0,8-N+1):
+            ans1 = arr[i][j:j+N]
+            ans2=ans1[::-1]
+            if ans1 == ans2:
+                ans +=1
+
+    for j in range(0,8):
+        for i in range(0,8-N+1):
+            ans3 =''
+            for k in range(i,i+N):
+                ans3 += arr[k][j]
+            ans4 = ans3[::-1]
+            if ans3 == ans4:
+                    ans+=1
+
+    print(f'#{tc} {ans}')
